27ce55f7dd7b4910aecd67f7d4c6566a2b34d5fe
Esteban Horn <estebanhxrn@gmail.com>
2024-08-24 22:59:02 -0300

    chore: Update database connection and add dotenv support
    This commit updates the `Connect` function in `database.go` to include support for loading environment variables from a `.env` file using the `godotenv` package. It also retrieves the MongoDB URI from the `MONGOURI` environment variable. This change improves the flexibility and security of the database connection setup.
    
    Additionally, the commit updates the `go.mod` and `go.sum` files to include the indirect dependency on the `github.com/joho/godotenv` package.
    
    Lastly, the commit removes unnecessary commented code and adds a `.gitignore` file to ignore certain files and directories.
    
    ```

--- database/database.go
+++ database/database.go
@@ -3,24 +3,42 @@ package database
 import (
 	"context"
 	"fmt"
-	//"go.mongodb.org/mongo-driver/bson"
+	"os"
+	"log"
+
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Connect establishes a connection to the MongoDB database and returns the client.
 func Connect() (*mongo.Client, error) {
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	
+	// Cargar el archivo .env
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("Error loading .env file: %v", err) // Usa Printf en lugar de Fatalf para no detener la ejecución
+	}
+
+	// Obtener la URI de MongoDB desde la variable de entorno
+	mongoURI := os.Getenv("MONGOURI")
+	if mongoURI == "" {
+		log.Fatal("MONGOURI environment variable is not set")
+	}
+
+	fmt.Println("Mongo URI: ", mongoURI)
+
+	// Configurar las opciones del cliente de MongoDB
 	opts := options.Client().
-		ApplyURI("mongodb+srv://estebanhxrn:4o6H3xrdKrvop0RY@recipes1.ras5f.mongodb.net/?retryWrites=true&w=majority&appName=Recipes1").
-		SetServerAPIOptions(serverAPI)
+		ApplyURI(mongoURI).
+		SetServerAPIOptions(options.ServerAPI(options.ServerAPIVersion1))
 
 	client, err := mongo.Connect(context.TODO(), opts)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to MongoDB: %v", err)
 	}
 
-	// Check the connection to MongoDB
+	// Verificar la conexión a MongoDB
 	if err := client.Ping(context.TODO(), nil); err != nil {
 		return nil, fmt.Errorf("failed to ping MongoDB: %v", err)
 	}
--- main.go
+++ main.go
@@ -2,25 +2,24 @@ package main
 
 import (
 	"fmt"
-	//"github.com/EstebanHorn/RecipesAPI/database"
-	"github.com/EstebanHorn/RecipesAPI/routes"
-	"net/http"
-	"github.com/gorilla/mux"
 	"log"
-	//"context"
+	"net/http"
+
+	"github.com/EstebanHorn/RecipesAPI/routes"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Recipes api")
 
 
+	// Configurar las rutas del servidor
 	r := mux.NewRouter()
 	routes.ConfigRoutes(r)
 
-	http.Handle("/", r)
+	// Iniciar el servidor
 	log.Println("Server running on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("Error starting server")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("Error starting server: %v", err)
 	}
-
 }
